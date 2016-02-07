d5b85c4d0d9921cb95ee9032386c47b8dec76d7f
timujin <newtimujin@gmail.com>
2016-02-07 16:43:33 +0300

    Now POST body from client is properly delivered

--- proxy.pl
+++ proxy.pl
@@ -18,29 +18,36 @@ sub http_connect($$) {
 			on_eof => sub {
 				$hdl->destroy;
 				$chdl->destroy;
-				warn "----RESPONSE FINISH----";
+				#warn "----RESPONSE FINISH----";
 			};
 
 		$hdl->push_write ("$s{method} $s{path} HTTP/1.1\015\012");
-		warn "-----REQUEST START-----";
-		warn ("$s{method} $s{path} HTTP/1.1\015\012");
+		#warn "-----REQUEST START-----";
+		#warn ("$s{method} $s{path} HTTP/1.1\015\012");
 
 		for (keys %{$s{headers}}) {
 			$hdl->push_write ("$_: $s{headers}{$_}\015\012");
-			warn "$_: $s{headers}{$_}\015\012";
+			#warn "$_: $s{headers}{$_}\015\012";
 		}
 		$hdl->push_write("\015\012");
-		warn("\015\012");
-		warn "-----REQUEST FINISH----";
 
-		warn "-----RESPONSE START----";
+		if ($s{method} eq "POST") {
+			$chdl->on_read(sub {
+				$hdl->push_write($_[0]->rbuf);
+				$_[0]->rbuf = "";
+			});
+		}
+		#warn("\015\012");
+		#warn "-----REQUEST FINISH----";
+
+		#warn "-----RESPONSE START----";
 		$hdl->push_read (line => "\015\012\015\012", sub {
 			my ($hdl, $line) = @_;
 			$chdl->push_write("$line\015\012\015\012");
-			warn ("$line\015\012\015\012");
+			#warn ("$line\015\012\015\012");
 			$hdl->on_read (sub {
 				$chdl->push_write($_[0]->rbuf);
-				warn ($_[0]->rbuf);
+				#warn ($_[0]->rbuf);
 				$_[0]->rbuf = "";
 			});
 		});
@@ -104,7 +111,7 @@ sub CONNECT($$) {
 
 tcp_server '127.0.0.1', '7777', sub {
 	my ($fh, $host, $port) = @_;
-	warn "Connect from $host:$port\n";
+	#warn "Connect from $host:$port\n";
 	my %s;
 	$fh = AnyEvent::Handle->new(
 		fh => $fh,
@@ -114,8 +121,8 @@ tcp_server '127.0.0.1', '7777', sub {
 		my ($self, $rbuf) = @_;
 		($s{start_line}, $s{headers}) = split "\r\n", $rbuf, 2;
 		unless ($s{start_line} =~ m{^(?<method>GET|POST|CONNECT) (?<path>[^ ]+) (?<proto>HTTP/1\.[01])$}) {
-			warn$s{start_line};
-			warn "400 Bad Request";
+			#warn$s{start_line};
+			#warn "400 Bad Request";
 			_400_bad_request($fh);
 			$fh->destroy;
 			return;
@@ -126,8 +133,8 @@ tcp_server '127.0.0.1', '7777', sub {
 		$s{headers} = {};
 		for (@h) {
 			unless(m{(?<name>[a-zA-Z0-9-]+): *(?<value>.*)}) {
-				warn $_;
-				warn "400 Bad Request";
+				#warn $_;
+				#warn "400 Bad Request";
 				_400_bad_request($fh);
 				$fh->destroy;
 				return;
@@ -136,28 +143,33 @@ tcp_server '127.0.0.1', '7777', sub {
 		}
 		if ($s{method} eq "CONNECT") {
 			unless ($s{path} =~ "^(?<host>[0-9a-zA-z-\.]+):443") {
-				warn $s{path};
-				warn "400 Bad Request";
+				##warn $s{path};
+				#warn "400 Bad Request";
 				_400_bad_request($fh);
 				$fh->destroy;
 				return;
 			}
 			$s{host} = $+{host};
-			warn Dumper \%s;
+			#warn Dumper \%s;
 			CONNECT(\%s, $fh);
 			return
 		}
 
 		if ($s{headers}{'Connection'}) { $s{headers}{'Connection'} = "close";}
 		unless ($s{path} =~ "http:\/\/(?<host>[0-9a-zA-z-\.]+)(?<path>\/.*)") {
-			warn $s{path};
-			warn "400 Bad Request";
+			#warn $s{path};
+			#warn "400 Bad Request";
 			_400_bad_request($fh);
 			$fh->destroy;
 			return;
 		}
+
+		#if ($s{method} eq "POST") {
+		#	$fh->on_read(sub { $s{client_body} .= $_[0]->rbuf; $_[0]->rbuf="";});
+		#}
+
 		($s{host}, $s{path}) = ($+{host}, $+{path});
-		warn Dumper \%s;
+		#warn Dumper \%s;
 		http_connect(\%s, $fh);
 	});
 };
